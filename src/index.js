ae57f25f78dba6021b1d6ed6568332ac8acb76a7
vagnermengali <vagmengali2015@gmail.com>
2022-10-27 23:10:16 -0300

    feat: adding error page refactoring

--- src/index.js
+++ src/index.js
@@ -5,20 +5,17 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom"
 import { GlobalStyles } from './style/global';
 import  { ResetCSS }  from './style/global';
-import ProviderLottie from './providers/lottieContext';
 import ProviderUser from './providers/userContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <ProviderLottie>
       <ProviderUser>
       <GlobalStyles />
       <ResetCSS />
       <App />
       </ProviderUser>
-    </ProviderLottie>
     </BrowserRouter>
   </React.StrictMode>
 );
