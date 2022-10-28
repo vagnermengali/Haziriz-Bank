f153908d80660ea5bdcb94359801fbb74813956f
vagnermengali <vagmengali2015@gmail.com>
2022-10-27 22:26:09 -0300

    feat: adding new routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,16 +2,19 @@ import Register from "../pages/Register"
 import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login"
 import Error from "../pages/Error"
+import Support from "../pages/Support";
+import Home from "../pages/Home";
 import {Routes, Route} from "react-router-dom";
 
 function RouteMain() {
 
     return (
     <Routes>
-        <Route path="/" element={<Login />}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/register" element={<Register />}></Route>
-        <Route path="/home/:id" element={<Dashboard />}></Route>
+        <Route path="/support" element={<Support />}></Route>
         <Route path="*" element={<Error replace to="/error"/>}></Route>
     </Routes>
     )
